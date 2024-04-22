1b3ee935bef5c952f6b20eacfe5e5302e82defb2
kamilek19dm <163117904+kamilek19dm@users.noreply.github.com>
2024-04-22 20:33:07 +0200

    Add files via upload

---
+++ Tutorial5/Controllers/AnimalsController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Tutorial5.Models;
+using Tutorial5.Models.DTOs;
+
+namespace Tutorial5.Controllers;
+
+[ApiController]
+// [Route("api/animals")]
+[Route("api/[controller]")]
+public class AnimalsController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    public AnimalsController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet]
+    public IActionResult GetAnimals(string? orderBy)
+    {
+        if (orderBy == null || orderBy.Length == 0)
+        {
+            orderBy = "name";
+        }
+
+        if (!new List<string>(){ "name", "description", "category", "area"}.Contains(orderBy))
+        {
+            return BadRequest();
+        }
+
+        // Otwieramy połączenie
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+        
+        // Definiujemy commanda
+        using SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "SELECT * FROM Animal ORDER BY " + orderBy + " ASC";
+
+        // Wykonanie commanda
+        var reader = command.ExecuteReader();
+        
+        List<Animal> animals = new List<Animal>();
+
+        int idAnimalOrdinal = reader.GetOrdinal("IdAnimal");
+        int nameOrdinal = reader.GetOrdinal("Name");
+
+        while (reader.Read())
+        {
+            animals.Add(new Animal()
+            {
+                IdAnimal = reader.GetInt32(idAnimalOrdinal),
+                Name = reader.GetString(nameOrdinal)
+            });
+        }
+        
+        return Ok(animals);
+    }
+
+    [HttpPost]
+    public IActionResult AddAnimal(AddAnimal animal)
+    {
+        // Otwieramy połączenie
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+        
+        // Definiujemy commanda
+        using SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "INSERT INTO Animal (name, description, category, area) VALUES(@animalName, @animalDesc, @animalCategory, @animalArea)";
+        command.Parameters.AddWithValue("@animalName", animal.Name);
+        command.Parameters.AddWithValue("@animalDesc", animal.Description);
+        command.Parameters.AddWithValue("@animalCategory", animal.Description);
+        command.Parameters.AddWithValue("@animalArea", animal.Area);
+
+        command.ExecuteNonQuery();
+        
+        return Created("", null);
+    }
+
+    [HttpPut]
+    public IActionResult UpdateAnimal(UpdateAnimal animal)
+    {
+        // Otwieramy połączenie
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+
+        // Definiujemy commanda
+        using SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "UPDATE Animal SET name=@animalName, description=@animalDesc, category=@animalCategory, area=@animalArea WHERE IdAnimal=@animalid";
+        command.Parameters.AddWithValue("@animalid", animal.AnimalID);
+        command.Parameters.AddWithValue("@animalName", animal.Name);
+        command.Parameters.AddWithValue("@animalDesc", animal.Description);
+        command.Parameters.AddWithValue("@animalCategory", animal.Description);
+        command.Parameters.AddWithValue("@animalArea", animal.Area);
+
+        command.ExecuteNonQuery();
+
+        return Ok(animal);
+    }
+
+    [HttpDelete]
+    public IActionResult DeleteAnimal(int animalID)
+    {
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+
+        // Definiujemy commanda
+        using SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "DELETE FROM Animal WHERE IdAnimal=@animalid";
+        command.Parameters.AddWithValue("@animalid", animalID);
+        command.ExecuteNonQuery();
+
+        return NoContent();
+    }
+}
---
+++ Tutorial5/Models/DTOs/AddAnimal.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tutorial5.Models.DTOs;
+
+public class AddAnimal
+{
+    [Required]
+    [MinLength(3)]
+    [MaxLength(200)]
+    public string Name { get; set; }
+    [MaxLength(200)]
+    public string? Description { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public string Category { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Area { get; set; }
+}
